009bc67db09d2ad8f915568ed722fc19f437820d
Your Name <raghurajramkumar927@gmail.com>
2026-02-27 16:23:42 +0530

    Fix valid Liquid syntax and add noreferrer in image.html snippet

--- js/customscripts.js
+++ js/customscripts.js
@@ -1,18 +1,18 @@
 $('#mysidebar').height($(".nav").height());
 
 
-$( document ).ready(function() {
+$(document).ready(function () {
 
     //this script says, if the height of the viewport is greater than 800px, then insert affix class, which makes the nav bar float in a fixed
     // position as your scroll. if you have a lot of nav items, this height may not work for you.
     var h = $(window).height();
     //console.log (h);
     if (h > 700) {
-        $( "#mysidebar" ).attr("class", "nav affix");
+        $("#mysidebar").attr("class", "nav affix");
     }
     // activate tooltips. although this is a bootstrap js function, it must be activated this way in your theme.
     $('[data-toggle="tooltip"]').tooltip({
-        placement : 'top'
+        placement: 'top'
     });
 
     /**
@@ -24,9 +24,9 @@ $( document ).ready(function() {
 
 // needed for nav tabs on pages. See Formatting > Nav tabs for more details.
 // script from http://stackoverflow.com/questions/10523433/how-do-i-keep-the-current-tab-active-with-twitter-bootstrap-after-a-page-reload
-$(function() {
+$(function () {
     var json, tabsState;
-    $('a[data-toggle="pill"], a[data-toggle="tab"]').on('shown.bs.tab', function(e) {
+    $('a[data-toggle="pill"], a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
         var href, json, parentId, tabsState;
 
         tabsState = localStorage.getItem("tabs-state");
@@ -41,11 +41,11 @@ $(function() {
     tabsState = localStorage.getItem("tabs-state");
     json = JSON.parse(tabsState || "{}");
 
-    $.each(json, function(containerId, href) {
+    $.each(json, function (containerId, href) {
         return $("#" + containerId + " a[href=" + href + "]").tab('show');
     });
 
-    $("ul.nav.nav-pills, ul.nav.nav-tabs").each(function() {
+    $("ul.nav.nav-pills, ul.nav.nav-tabs").each(function () {
         var $this = $(this);
         if (!json[$this.attr("id")]) {
             return $this.find("a[data-toggle=tab]:first, a[data-toggle=pill]:first").tab("show");
